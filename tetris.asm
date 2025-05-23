0cde61b692affe093d83140fb5586eae6f88af0b
kioshiotani <brunokioshiotani@gmail.com>
2025-05-23 15:36:20 -0300

    Adicionada a função wait_start

--- tetris.asm
+++ tetris.asm
@@ -36,3 +36,64 @@ mapa29 : string "                                        "
 pos     : var #1
 pos_ant : var #1
 
+;se 1, spawn
+flag_spawn : var #1
+static flag_spawn, #1 ;inicializacao
+
+;indice randomico
+rand_index : var #1
+
+
+main:
+	loadn r0, #499 ;contador para pegar indice aleatorio
+	loadn r1, #' ' ;para verificar se o jogador iniciou o jogo
+	
+	call wait_start
+
+	halt
+
+;--------------------------------------------------
+;wait_start
+;--------------------------------------------------
+;espera o jogador apertar ESPACO, enquanto ele nao apertar um contador ficara sendo incrementado
+;para coletar um indice aleatorio do vetor randomico
+wait_start:
+	;salvar os valores de FR, r0 e r1
+	push FR
+	push r0
+	push r1
+	push r2
+
+	loadn r0, #499 ;contador
+	loadn r1, #' ' ;para verificar se o jogador pressionou start
+
+	loop_count:
+		dec r0
+		jz reset_count ;caso o contador zere, resetar
+		inchar r2
+		cmp r1, r2
+		jeq end_wait_start
+		jmp loop_count
+
+	reset_count:
+		loadn r0, #499
+		jmp loop_count
+
+	end_wait_start:
+		store rand_index, r0 ;salva o indice randomico
+		pop r2
+		pop r1
+		pop r0
+		pop FR
+		rts
+;--------------------------------------------------
+;END wait_start
+;--------------------------------------------------
+
+
+
+
+
+
+
+
